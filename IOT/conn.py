4b5c8e900c31d780750383d503a6bdc9d9bfb882
Vinicius farineli freire <64615200+Farivini@users.noreply.github.com>
2021-11-05 20:54:55 -0300

    Update conn.py

--- IOT/conn.py
+++ IOT/conn.py
@@ -1,5 +1,5 @@
-usuario = 'f654e590-1d79-11ec-8da3-474359af83d7'
-ss = 'e2d7154a70f031a442bae542842ae9b88e575fe6'
-id = '0a214190-1d7a-11ec-bbfc-979c23804144'
+usuario = ''
+ss = ''
+id = ''
 server = 'mqtt.mydevices.com'
 porta = 1883

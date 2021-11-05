20fc0251dab29e6beb24136a1c641ffd9870b47e
Vinicius farineli freire <64615200+Farivini@users.noreply.github.com>
2021-11-05 20:54:23 -0300

    Update aviso_telegram.py

--- IOT/aviso_telegram.py
+++ IOT/aviso_telegram.py
@@ -1,7 +1,7 @@
 import telegram
 
-my_token = '2025583090:AAEGG1TvDck5dOoD9g5KYV_hFFPrtPya39U'
-chat_id = '-513797981'
+my_token = 'seu token'
+chat_id = 'seu id'
 link = 'https://cayenne.mydevices.com/cayenne/dashboard/device/0a214190-1d7a-11ec-bbfc-979c23804144'
 
 def envia(msg, chat_id, token=my_token):
